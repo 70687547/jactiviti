f645f6762d75b48c06e487a3581ad572624fe040
姚灵程 <jianx70687547@gmail.com>
2020-08-25 11:00:00 +0800

    Initial commit

---
+++ src/test/java/top/jianx/activiti/JactivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jianx.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JactivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
